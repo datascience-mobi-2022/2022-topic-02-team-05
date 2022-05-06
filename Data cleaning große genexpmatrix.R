71734b5341536560e65b2a67f9274b2c217b30db
JakobThen <104084885+JakobThen@users.noreply.github.com>
2022-05-06 15:37:50 +0200

    biotype plotting

--- Data cleaning große genexpmatrix.R
+++ Data cleaning große genexpmatrix.R
@@ -1,3 +1,20 @@
+#wichtige packages
+#install.packages("msigdbr")
+# if (!require("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# 
+# BiocManager::install("biomaRt")
+library(biomaRt)
+library(msigdbr)
+library(dplyr)
+library(ggplot2)
+#laden der daten von carl
+#tcga_exp = readRDS("C:/Users/jakob/Documents/R/Data Analysis Seminar/tcga_tumor_log2TPM.RDS")
+#tcga_annot = readRDS("C:/Users/jakob/Documents/R/Data Analysis Seminar/tcga_tumor_annotation.RDS")
+#tcga_tumor_norm = readRDS("C:/Users/jakob/Documents/R/Data Analysis Seminar/tcga_tumor_normal_datascience_proj_2020.RDS")
+#genesets = readRDS("C:/Users/jakob/Documents/R/Data Analysis Seminar/hallmarks_genesets.rds")
+
+
 #----------------------------------------
 #preprocessing of tcga gene exp data
 #----------------------------------------
@@ -18,18 +35,13 @@ tcga_exp_hvar = tcga_exp_narm[log(tcga_exp_var) > -1, ]
 #auswahl nicer pathways
 #------------------------------
 
-#install.packages("msigdbr")
-# if (!require("BiocManager", quietly = TRUE))
-#   install.packages("BiocManager")
-# 
-# BiocManager::install("biomaRt")
-
 #loading our selected pathways
 gsea_pathways = msigdbr(species = "Homo sapiens")
 TERT_pathway = gsea_pathways[gsea_pathways$gs_name == "BIOCARTA_TEL_PATHWAY", ]
 MAPK_pathway = gsea_pathways[gsea_pathways$gs_name == "BIOCARTA_MAPK_PATHWAY", ]
 thyroidhormone_pathway = gsea_pathways[gsea_pathways$gs_name == "GOBP_THYROID_HORMONE_METABOLIC_PROCESS", ]
 
+#liste aller pathways mit allen inofs zum jeweiligen pathway
 our_genesets = list(TERT_pathway$gene_symbol,
                     MAPK_pathway$gene_symbol,
                     thyroidhormone_pathway$gene_symbol
@@ -37,22 +49,99 @@ our_genesets = list(TERT_pathway$gene_symbol,
 names(our_genesets) = c('TERT_pathway', 'MAPK_pathway', 'thyroidhormone_pathway')
 
 
+
 #---------------------------------------------------
 #genetypen der pathways checken
-#alles zur biomart function hab ich hier her:
-#https://cran.r-project.org/web/packages/biomartr/vignettes/Functional_Annotation.html
 #---------------------------------------------------
 
 #function die Gennamen nimmt und dafür den gentypen gibt
-checkgenetypes = function(pathway){biomart(pathway,
-                                           mart = "ENSEMBL_MART_ENSEMBL",
-                                           dataset = "hsapiens_gene_ensembl",
-                                           attributes = c("description","gene_biotype","transcript_biotype"),
-                                           filters = "external_gene_name")
-                                   message('Let me check that for you :D')   
+checkbiotypes = function(pathway){mart = useEnsembl(dataset = "hsapiens_gene_ensembl", biomart='ensembl')
+  
+                                  res = getBM(attributes = c("ensembl_gene_id", "gene_biotype"),
+                                        filters = "external_gene_name",
+                                        values = pathway,
+                                        mart = mart )
+                                  message('Let me check that for you :D')
+                                  
+                                  return(res)
                                   }
-TERT_genetypes = checkgenetypes(TERT_pathway$gene_symbol)
+#-----------------------------------------
+#gene biotypes unserer pathways bestimmen
+#-----------------------------------------
+our_genesets_biotypes = sapply(our_genesets, checkbiotypes)
+#entfernen der ids
+our_genesets_biotypes = our_genesets_biotypes[2,]
 
 
 
+
+#function die alle biotypes eines pathways als barplot plottet
+#genelist ist dabei die liste unserer pathways, number die stelle des pathways in der liste
+plotbiotypes = function(genelist, number){
+  x = as.data.frame(table(as.vector(genelist[[number]])))
+  colnames(x) = c('Biotype','Ammount')
+  
+  ggplot(x, aes(Biotype, Ammount)) + geom_bar(stat = 'identity') +  
+    labs(title = names(genelist[number])) +
+    theme(axis.text = element_text(angle = 0,))
+}
+
+#plotten aller unserer pathways
+#funktioniert auch noch nicht!!!
+par(mfrow=c(1,3))
+for (i in 1:length(our_genesets_biotypes)) {
+  plot = plotbiotypes(our_genesets_biotypes, i)
+  
+}
+
+
+#-------------------------------
+#gene biotypes von carls pathways bestimmen
+#----------------------------------
+
+#DAUERT EWIG!!!!!!!
+genesets_biotypes = sapply(genesets[[1]], checkbiotypes)
+#entfernen der ids
+genesets_biotypes = genesets_biotypes[2,]
+
+
+#plotten des ersten von carls pathways
+plotbiotypes(genesets_biotypes, 1)
+
+
+
+
+#---------------------------------------------------------
+#analyse der biotypen der gene aus den tcga expressionsdaten
+#---------------------------------------------------------
+
+#gennamen extrahieren
+tcga_genes = rownames(tcga_exp_hvar)
+
+#dieser vetor enthält sowohl enseblm id als auch genenamen und muss daher gespalten werden
+tcga_genes = strsplit(tcga_genes, split = '|', fixed = TRUE)
+
+#speicher der gennamen als eigenen vektor
+tcga_genenames = sapply(tcga_genes, function(tcga_genes){return(tcga_genes[2])})
+
+#entfernen der Versionsnummern für ensembl id und gennamen
+tcga_genenames = strsplit(tcga_genenames, split = '.', fixed = TRUE)
+tcga_genenames = sapply(tcga_genenames, function(tcga_genenames){return(tcga_genenames[1])})
+
+#biotypes der tcga gene bestimmen
+tcga_biotypes = checkbiotypes(tcga_genenames)
+
+#darstellen aller biotypes im set
+x = as.data.frame(table(as.vector(tcga_biotypes$gene_biotype)))
+colnames(x) = c('Biotype','Ammount')
+
+ggplot(x, aes(Biotype, Ammount)) + geom_bar(stat = 'identity') +  
+  labs(title = 'TCGA expression data biotypes') +
+  theme(axis.text = element_text(angle = 90,))
+
+
+#--------------------------------------------------------------------------
+#entfernen aller nicht proteincodierenden gene aus der tcga expressions matrix
+#--------------------------------------------------------------------------
+tcga_exp_protcod = tcga_exp_hvar[tcga_biotypes$gene_biotype == 'protein_coding', ]
 
