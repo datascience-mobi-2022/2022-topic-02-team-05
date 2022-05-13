be75e8c556ba378187705f807fa7439047a3f920
JakobThen <jakob.then@stud.uni-heidelberg.de>
2022-05-13 23:31:20 +0200

    Update 01_GSEA of TCGA exp.R

--- pan_cancer/01_GSEA of TCGA exp.R
+++ pan_cancer/01_GSEA of TCGA exp.R
@@ -1,6 +1,6 @@
 #--------------------------------------------------
 # In diesem Dukoment versuchen wir die Gereinigten expressionsdaten mittels gsea zu
-#pathway activitäten zusammenzufassen un din einer matrix mit patienten als spalten und 
+#pathway activit?ten zusammenzufassen un din einer matrix mit patienten als spalten und 
 # enrichment scores als zeilen zusammenzufassen
 #--------------------------------------------------
 
@@ -15,39 +15,40 @@ library(pheatmap)
 
 load('~/GitHub/2022-topic-02-team-05/data/tcga_exp_cleaned.RData')
 load('~/GitHub/2022-topic-02-team-05/data/our_genesets.RData')
-genesets = readRDS("~/GitHub/2022-topic-02-team-05/data/hallmarks_genesets.rds")
+load("~/GitHub/2022-topic-02-team-05/data/geneset_ids.RData")
 
-#----------------------------------------------------------
-#bennen der gene in tcga exp nur mit namen ohne ensembl id
-#--------------------------------------------------------
-#gennamen extrahieren
-tcga_genes = rownames(tcga_exp_cleaned)
-
+#--------------------------------
+#Extraktion aller ensembl ids und gennamen aus den cleanen exp daten
+#--------------------------------
+tcga_genes_cleaned = rownames(tcga_exp_cleaned)
 #dieser vetor enth?lt sowohl enseblm id als auch genenamen und muss daher gespalten werden
-tcga_genes = strsplit(tcga_genes, split = '|', fixed = TRUE)
+tcga_genes_cleaned = strsplit(tcga_genes_cleaned, split = '|', fixed = TRUE)
+#speicher der ensembl ids ohne versionsnummer als eigenen vektor
+tcga_geneids = sapply(tcga_genes_cleaned, function(tcga_genes_cleaned){return(tcga_genes_cleaned[1])})
+tcga_geneids = strsplit(tcga_geneids, split = '.', fixed = TRUE)
+tcga_geneids = sapply(tcga_geneids, function(tcga_geneids){return(tcga_geneids[1])})
 
-#speicher der gennamen als eigenen vektor
-tcga_genenames = sapply(tcga_genes, function(tcga_genes){return(tcga_genes[2])})
-
-#entfernen der Versionsnummern f?r ensembl id und gennamen
+#speicher der genenames ohne versionsnummer als eigenen vektor
+tcga_genenames = sapply(tcga_genes_cleaned, function(tcga_genes_cleaned){return(tcga_genes_cleaned[2])})
 tcga_genenames = strsplit(tcga_genenames, split = '.', fixed = TRUE)
 tcga_genenames = sapply(tcga_genenames, function(tcga_genenames){return(tcga_genenames[1])})
+
+tcga_genes_cleaned = cbind.data.frame(tcga_geneids,tcga_genenames)
+#speichern eines datframes der der die Ensembl ids und genenamen aller genen der exp daten enth?lt
+save(tcga_genes_cleaned, file = '~/GitHub/2022-topic-02-team-05/data/tcga_genes_cleaned.RData')
 
 #----------------------
 #versuch eine gsea zu implementieren
 #------------------------
 
-#function die eine gsea für einen beliebigen patienten durchführt und nur den NES benannt ausgibt
-#alle pathways die nocht siginifikant von der base level abweichen werden 0 gesetzt
-
-GSEA_NES = function(patient, pathways = c(genesets[[1]], our_genesets), pvalue = 0.05){
+#function die eine gsea f?r einen beliebigen patienten durchf?hrt und nur den NES benannt ausgibt
+GSEA_NES = function(patient){
   
   #sorting and naming the exp data
-  names(patient) = tcga_genenames
-  #hiervor z-transforamtion einfügen
+  names(patient) = tcga_genes_cleaned$tcga_geneids
   patient = sort(patient, decreasing = TRUE) 
   
-  #duarchführen der GSEA
+  #duarchf?hren der GSEA
   res = fgseaMultilevel(pathways = pathways, 
                         stats = patient,
                         minSize=3)
@@ -56,35 +57,41 @@ GSEA_NES = function(patient, pathways = c(genesets[[1]], our_genesets), pvalue =
   #ret[res$padj > pvalue] = 0
   ret[is.na(ret)] = 0
   
-  message('I´m still standing')
+  message('I?m still standing')
   return(ret)
 }
 
 #-------------------------------------------
-#Durchführung der GSEA
+#Durchf?hrung der GSEA
 #-----------------------------------------------
-
 #pathways = gmtPathways("C:/Users/jakob/Desktop/c2.cp.v7.5.1.symbols.gmt") alle canonischen pathways msigr vllt als alternative
-GSEA_matrix = apply(tcga_exp_cleaned[,1:20], 2, GSEA_NES) %>% as.data.frame()
+pathways = c(genesets_ids, our_genesets)
+#pvalue = 0.05
+
+#ztransforamtion der daten zur gsea analyse nach Peng et al.
+tcga_exp_ztrans = apply(as.matrix(tcga_exp_cleaned), 2, scale)
+
+GSEA_matrix = apply(tcga_exp_ztrans[,1:100], 2, GSEA_NES) %>% as.data.frame()
+save(GSEA_matrix, file = '~/GitHub/2022-topic-02-team-05/data/GSEA_matrix.RData')
 
 #darstellen der GSEA matrix als heatmap
-#rote sind überexpremierte pathways, blue unterexpremierte, weiß keine abweichung
+#rote sind ?berexpremierte pathways, blue unterexpremierte, wei? keine abweichung
 pheatmap(as.matrix(GSEA_matrix),
          breaks = seq(-max(GSEA_matrix), max(GSEA_matrix), length.out = 201),
-         color = colorRampPalette(c('blue','light blue', 'white','yellow', 'red'),
+         color = colorRampPalette(c('blue','lightskyblue','lightblue1', 'white','lightyellow','yellow', 'red'),
                                   bias = 1,
                                   space = 'rgb',
                                   interpolate = 'linear'
          )(200),
          clustering_method = 'average',
-         treeheight_row = 25, treeheight_col = 20, cellwidth = 20,cellheight = 20,
-         show_colnames = FALSE,
+         treeheight_row = 25, treeheight_col = 20, cellwidth = 7,cellheight = 8,
+         show_colnames = FALSE, fontsize_row = 8, border_color = NA,
          legend_breaks = c(-max(GSEA_matrix),0, max(GSEA_matrix)),
          legend_labels = c('underexpressed', 'normal expression', 'overexpressed')
         )
 
 
-save(GSEA_matrix, file = '~/GitHub/2022-topic-02-team-05/data/GSEA_matrix.RData')
+
 
 
 
