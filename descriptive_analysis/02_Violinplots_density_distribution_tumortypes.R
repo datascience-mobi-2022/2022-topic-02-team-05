3a784ed374bc0301ae8292eff6ffd5cca785f4c4
annamandel <anna.lange2001@web.de>
2022-05-15 11:41:42 +0200

    Loading cleaned data for violin plots

---
+++ descriptive_analysis/02_Violinplots_density_distribution_tumortypes.R
@@ -0,0 +1,6 @@
+#-------------------------------------------------------------------------------
+# Darstellung der Verteilung der Genexpressions-Verteilung (density plot) für 
+# jeden unserer 5 Krebstypen: Darstellung über violin plots
+#-------------------------------------------------------------------------------
+
+load('~/GitHub/2022-topic-02-team-05/data/tcga_exp_cleaned.RData')
