f7c669cd45c4345e8ab53d5220442e9f27df3af4
annamandel <anna.lange2001@web.de>
2022-05-15 14:02:37 +0200

    Violinplots implementiert

--- descriptive_analysis/02_Violinplots_density_distribution_tumortypes.R
+++ descriptive_analysis/02_Violinplots_density_distribution_tumortypes.R
@@ -3,4 +3,40 @@
 # jeden unserer 5 Krebstypen: Darstellung über violin plots
 #-------------------------------------------------------------------------------
 
+#Daten laden:
 load('~/GitHub/2022-topic-02-team-05/data/tcga_exp_cleaned.RData')
+tcga_anno = readRDS("~/GitHub/2022-topic-02-team-05/data/tcga_tumor_annotation.RDS")
+
+#Packages:
+install.packages("vioplot")
+library("vioplot")
+
+#Patienten herauspicken nach den einzelnen Tumortypes:
+BRCA_genes <- tcga_exp_cleaned[,tcga_anno$cancer_type_abbreviation == 'BRCA']
+  
+KIRC_genes <- tcga_exp_cleaned[,tcga_anno$cancer_type_abbreviation == 'KIRC']
+
+LUAD_genes <- tcga_exp_cleaned[,tcga_anno$cancer_type_abbreviation == 'LUAD']
+
+PRAD_genes <- tcga_exp_cleaned[,tcga_anno$cancer_type_abbreviation == 'PRAD']
+
+THCA_genes <- tcga_exp_cleaned[,tcga_anno$cancer_type_abbreviation == 'THCA']
+
+#rbind(BRCA_genes, KIRC_genes,LUAD_genes,PRAD_genes,THCA_genes)
+
+#Mittelwerte der einzelnen Gene:
+BRCA_mean <- apply(BRCA_genes,1, mean)
+KIRC_mean <- apply(KIRC_genes,1, mean)
+LUAD_mean <- apply(LUAD_genes,1, mean)
+PRAD_mean <- apply(PRAD_genes,1, mean)
+THCA_mean <- apply(THCA_genes,1, mean)
+
+#Violinplots mit einzelnen Tumortypes:
+
+vioplot(BRCA_mean)
+
+#alle Violinplots in einem Plot! 
+#Achsen noch beschriften
+
+
+  
