3ff89e2b22a7d9975f4aa67f35fb680be3aeedba
JakobThen <jakob.then@stud.uni-heidelberg.de>
2022-07-04 15:57:42 +0200

    PCA und UMAP der THCA Exp Daten

--- thca_normal_vs_tumor/02_THCA PCA and UMAP.R
+++ thca_normal_vs_tumor/02_THCA PCA and UMAP.R
@@ -41,3 +41,21 @@ ggplot(UMAP_data, aes(x = V1, y = V2, color = stage, shape = type)) + geom_point
   theme(legend.key = element_rect(fill = 'white')) +
   theme_minimal() +
   guides(color = guide_legend(override.aes = list(size = 5)))
+
+#--------------------------------------
+#Zum Vergleichen führen wir nun auch PCA und UMAP auf die reinen Exp Daten durch
+#Dau verwenden wir den log2FC
+#--------------------------------------
+set.seed(123)
+load('data/thca_normal_exp_cleaned.RData')
+load('data/thca_tumor_exp_cleaned.RData')
+
+#log2FC für jeden Expressionwert berrechnen
+geneFC = thca_tumor_exp_cleaned - thca_normal_exp_cleaned
+
+#PCA
+PCA_genes = prcomp(t(geneFC))
+PCA_genes_data = as.data.frame(PCA$x)
+PCA_data$type = sapply(thca_anno$histological_type, FUN = function(x){return(strsplit(x, split = '-', fixed = TRUE)[[1]][2])})
+
+
