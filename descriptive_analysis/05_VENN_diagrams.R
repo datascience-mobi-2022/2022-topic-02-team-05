d55a052a1fafd91e1b7e1e0aadab8c0651320264
annamandel <anna.lange2001@web.de>
2022-07-06 13:36:20 +0200

    Update 05_VENN_diagrams.R

--- descriptive_analysis/05_VENN_diagrams.R
+++ descriptive_analysis/05_VENN_diagrams.R
@@ -8,6 +8,7 @@ library("VennDiagram")                                # Load VennDiagram package
 
 load("~/GitHub/2022-topic-02-team-05/data/tcga_genes_cleaned.RData")
 load("~/GitHub/2022-topic-02-team-05/data/tcga_exp_cleaned.RData")
+load("~/GitHub/2022-topic-02-team-05/data/pathways.RData")
 
 #------------------------------------------------------------------------
 # unlisting der Gennamen von Expressionsdaten
@@ -15,33 +16,30 @@ load("~/GitHub/2022-topic-02-team-05/data/tcga_exp_cleaned.RData")
 
 list <- rownames(tcga_exp_cleaned)
 ensemble_vector_tcga <- unlist(list)
-#identische müssen noch rausgeworfen werden
+
+#identische EnsembleIDS rauswerfen
+ensemble_vector_tcga <- unique(ensemble_vector_tcga) #kein Gen wird rausgeworfen! (Alle unterschiedlich)
 
 #-----------------------------------------------------------------------
 # Gennamen aus unseren Pathways extrahieren und in einen Vektor packen
 #-----------------------------------------------------------------------
-
-
-ensemble_vector_pathways <- 
+pathw <- unlist(pathways, use.names = FALSE)
+ensemble_vector_pathways <- pathw
   
 #-----------------------------------------------------------------------
 # Gennamen finden, die in Pathways und in Expressionsdaten sind
 #-----------------------------------------------------------------------  
-ensemble_vector_both <- c()
-
-for (i in length(ensemble_vector_tcga)){
-  a <- is.element(i, ensemble_vector_pathways)
-  if (a == TRUE) {
-    append(ensemble_vector_both, ensemble_vector[i])
-  }
-}
+ensemble_vector_both <- intersect(ensemble_vector_pathways, ensemble_vector_tcga)
 
 #-----------------------------------------------------------------------
 # VENN-Diagramm machen
 #-----------------------------------------------------------------------
 
 grid.newpage()                                        # Move to new plotting page
-draw.pairwise.venn(area1 = ensemble_vector_tcga,      # Create pairwise venn diagram
-                   area2 = ensemble_vector_pathways,
-                   cross.area = ensemble_vector_both)
+draw.pairwise.venn(area1 = length(ensemble_vector_tcga),      # Create pairwise venn diagram
+                   area2 = length(ensemble_vector_pathways),
+                   cross.area = length(ensemble_vector_both),
+                   fill = c("pink", "orange"),
+                   lty = "blank",
+                   category = c("TCGA", "Pathways"))
 
